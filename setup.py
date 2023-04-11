c69852fdbd95e46c261ca4ebb21bff539cf6b1be
God <godsoft@pm.me>
2023-04-11 16:05:12 +0300

    add requirements.txt

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='FormParser',
+    version='1.0.0',
+    packages=[''],
+    url='',
+    license='',
+    author='CyberParadise',
+    author_email='',
+    description='Parse form and inputs from Html'
+)
